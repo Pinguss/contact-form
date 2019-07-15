cf53f1c2fa4d67d6f23891de0016ae4212ef20f3
Pinguss <47196337+Pinguss@users.noreply.github.com>
2019-07-16 00:51:33 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,32 +1,4 @@
-       <?php   
 
-mb_internal_encoding("UTF-8");
-
-    $hlaska = '';
-    if ($_POST)
-    {
-        if (isset($_POST['jmeno']) && $_POST['jmeno'] &&
-			isset($_POST['email']) && $_POST['email'] &&
-			isset($_POST['message']) && $_POST['message'] &&
-			isset($_POST['antispam']) && $_POST['antispam'] == date('Y'))
-        {
-            $hlavicka = 'From:' . $_POST['email'];
-            $hlavicka .= "\nMIME-Version: 1.0\n";
-            $hlavicka .= "Content-Type: text/html; charset=\"utf-8\"\n";
-            $adresa = 'vas@email.cz';
-            $predmet = 'Nová zpráva z mailformu';
-            $uspech = mb_send_mail($adresa, $predmet, $_POST['message'], $hlavicka);
-            if ($uspech)
-            {
-                $hlaska = 'Email byl úspěšně odeslán, brzy vám odpovíme.';
-            }
-            else
-                $hlaska = 'Email se nepodařilo odeslat. Zkontrolujte adresu.';
-        }
-        else 
-            $hlaska = 'Formulář není správně vyplněný!';
-    }
-    ?>
 
 <!DOCTYPE html>
 
