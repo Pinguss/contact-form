091b5239f0d0aaba686d6441573a0fdd7365a3ca
Pinguss <47196337+Pinguss@users.noreply.github.com>
2019-07-15 23:55:12 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,66 @@
+        <?php
+        mb_internal_encoding("UTF-8");
+         $hlaska = '';
+         
+        if($_POST) 
+        {
+            
+          if(isset($_POST['jmeno']) && $_POST['jmeno'] && 
+            isset($_POST['email']) && $_POST['email'] && 
+            isset($_POST['message']) && $_POST['message'] && 
+            isset($_POST['antispam']) && $_POST['antispam'] == date('Y'))
+            {
+                $hlavicka = 'From:' . $_POST['email'];
+                $hlavicka .= "\nMIME-Version: 1.0\n";
+                $hlavicka .= "Content-Type: text/html; charset=\"utf-8\"\n";
+                $adresa = 'janope@seznam.cz';
+                $predmet = 'Nová zpráva z mailformu';
+                $uspech = mb_send_mail($adresa, $predmet, $_POST['zprava'], $hlavicka);
+if ($uspech)
+{
+        $hlaska = 'Email byl úspěšně odeslán, brzy vám odpovíme.';
+}
+else
+        $hlaska = 'Email se nepodařilo odeslat. Zkontrolujte adresu.';
+            }
+          else
+                $hlaska = 'Formulář není správně vyplněn!';
+        }
+        ?>
+
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Kontaktní formulář</title>
+    </head>
+    <body>
+        
+        <h1>Kontaktní formulář</h1>
+        <?php
+        if ($hlaska)
+                echo('<p>' . $hlaska . '</p>');
+?>
+        <form method="POST">
+            <label for="jmeno">Vaše jméno:</label>
+            <p><input id="jmeno" name="jmeno" type="text" /></p>
+            
+            <label for="email">E-mail:</label>
+            <p><input id="email" name="email" type="email"/></p>
+            
+            <label for="message">Zpráva:</label>
+            <p><textarea id="message" name="message" rows="5"></textarea></p>
+            
+            <label for="antispam">Zadejte aktuální rok:</label>
+            <p><input id="antispam" name="antispam" type="date"/></p>
+            
+            <input type="submit" value="Odeslat" />
+        </form>
+
+    </body>
+</html>
